456987a6ca178a55ff2a0a0ccaecb0b9953c7a16
HesanChavoshi <hesanchavoshi@Gmail.com>
2024-03-15 21:04:47 +0330

    class Character added

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Character {
+
+protected:
+    string name;
+    int age;
+    string gender;
+    int HP;
+    int Stamina;
+    int level;
+
+public:
+    Character(string name, int age, string gender) : name(name), age(age), gender(gender) {}
+
+    
+};
+
+int main() {
+
+    return 0;
+}
