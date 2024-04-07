4d25600fd5d8fd4fe8297d139e1f68a01f1039e4
amiralimaroufi82 <128319452+amiralimaroufi82@users.noreply.github.com>
2024-04-07 13:05:01 +0330

    Add files via upload

---
+++ mvc1.cpp
@@ -0,0 +1,60 @@
+// mvc.cpp
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+// enemies' data
+class Model {
+
+
+};
+
+
+
+class View {
+
+
+
+
+};
+
+
+
+public class Controller {
+
+    private  Model& model;
+    private  View& View;
+
+    public Controller(Model model ,View View)
+    {
+        _model= model;
+        _view= view;
+
+    }
+
+    public void Getting_enemies_data ()
+    {       // get the enemies data
+
+        _model.name = "...";
+
+        //....
+        
+        //_view.function in view's class(_model)
+
+    }
+
+};
+
+
+ var (model) = new Model ;
+ var  (view) = new View;
+ var (controller)= new  Controller(model, view);
+
+
+//controller.functions in Controller's class
+
+int main() {
+
+
+}
